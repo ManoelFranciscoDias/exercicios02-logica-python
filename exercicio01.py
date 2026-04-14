678dea05bea3931f35a1ebe56e4828399ccad99d
ManoelFranciscoDias <manoel2007dias@gmail.com>
2026-04-14 20:15:38 -0300

    ajuste na formatação das expressões de cálculo da área e perímetro do quadrado

--- exercicio01.py
+++ exercicio01.py
@@ -2,8 +2,8 @@
 lado = float(input("Digite o valor do lado do quadrado: "))
 
 #processamento
-area = lado ** 2
-perimetro = 4 * lado
+area = (lado ** 2)
+perimetro = (4 * lado)
 
 #saida
 print("A área do quadrado é:", area)
